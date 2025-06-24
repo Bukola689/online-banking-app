51422597f55600030fd0a2d931bb6fbbef022f10
Bukola689 <jbukola95@gmail.com>
2025-06-24 18:44:01 +0100

    add DTO Interface

--- app/Interfaces/DtoInterface.php
+++ app/Interfaces/DtoInterface.php
@@ -5,9 +5,14 @@ namespace App\Interfaces;
 interface DtoInterface
 {
 
+    // an aapi form request is a request that is used to validate and transform incoming data
+    // before it is passed to the controller. It is typically used in API endpoints to ensure
     public static function fromApiFormRequest():self;
 
+    // that the data is in the correct format and meets the required validation rules.
     public static function fromModel(Model $model): self;
 
+    // Convert the DTO to an array representation.
+    // This is useful for serializing the DTO to JSON or for returning it in API responses
     public static function toArray(Model $model): self;
 }
